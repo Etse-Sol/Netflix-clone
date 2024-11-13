7703980a1a7dc70c7405fb4b930c0bd084d574a9
Etse-Sol <solomonetsegenet7@gmail.com>
2024-11-13 13:09:32 +0300

    banner

--- Netflix-clone/src/pages/Home/Home.jsx
+++ Netflix-clone/src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../../Component/Header/Header'
 import Footer from '../../Component/Footer/Footer'
+import Banner from '../../Component/Banner/Banner'
 
 const Home = () => {
   return (
     <>
       <div>
         < Header/>
+        < Banner/>
         < Footer/>
+
         
       </div>
     </>
